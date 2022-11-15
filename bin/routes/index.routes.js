410cb0bc13e674f1101393cc378d45333d553982
alafsasa <alafsasa@gmail.com>
2022-11-15 20:22:13 +0300

    routes

---
+++ bin/routes/index.routes.js
@@ -0,0 +1,15 @@
+const controller = require('../controllers/auth.controller');
+
+module.exports = function(app){
+    app.use(function(res, req, next){
+        res.header(
+            "Access-Control-Allow-Headers",
+            "x-access-token, Origin, Content-Type, Accept"
+        );
+        next();
+    });
+    app.post(
+        "/api/auth/signup",
+        controller.signup
+    );
+}
