70383f56b87fb88e19501767727c1a1dc33e31b9
alafsasa <alafsasa@gmail.com>
2022-11-25 17:02:29 +0300

    added body-parser to accept POST data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const db = require('./bin/config/db.config');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 3001;
@@ -25,10 +26,19 @@ app.get('/', (req, res) => {
     res.send(post);
 });
 
+//parse requests of content-type-application/json
+app.use(bodyParser.json());
+//parse requests of content-type-application/x-www/form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//app routes
+require('./bin/routes/index.routes')(app);
+
 //start
 app.listen(PORT, ()=>{
     console.log(`Kwenik running on PORT: ${PORT}`);
 });
+
 
 //coonect to DB
 //clean db on every start
